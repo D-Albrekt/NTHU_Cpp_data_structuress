561e547217022a778e2a80d06d4f6861bfe07368
David Albrekt <albrektdavid@gmail.com>
2024-01-11 22:31:34 +0800

    Problem 1 testcase implemented, testcasewriter updated

--- Project/final_update_0103/testcase_writer.cpp
+++ Project/final_update_0103/testcase_writer.cpp
@@ -2,13 +2,58 @@
 
 using namespace std;
 
-#define P1_Vertices 5
+#define P1_Vertices 100
 #define P2_Vertices 100
 #define P1_edges 7
 
 void problem1()
 {
-    int i = 1+1;
+    cout << P1_Vertices << ' ' << (P1_Vertices * (P1_Vertices - 1))/2 << endl;
+    for (int i = 1; i <= P2_Vertices; i++)
+    {
+        for (int j = i + 1; j <= P2_Vertices; j++)
+        {
+            if (i != j)
+            {   
+                cout << i << ' '  << j  << ' ';
+                if (j%10 == 0)
+                {
+                    cout << 15 << ' '<< 10 << endl;
+                }else{
+                    cout << 10*(j%10) << ' ' <<  (((2*j)%15) + 5) << endl;
+                }
+            }
+        }
+    }
+    cout << endl; 
+    int request_id{1};
+    int source{1};
+    for (int i = 10; i < 95000; i++)
+    {
+        cout << "insert" << ' ' << request_id++ << ' ';
+        if (i%100 != 0)
+        {
+            source = i%100;
+        }
+        else{
+            source = 100;
+        }
+        cout << source <<  ' ' << "{ ";
+        for (int j = 1; j <= P1_Vertices; j++)
+        {
+
+            cout <<  j << ' ';
+            
+        }
+        cout << "} " << "10" << endl; 
+        if (i % 1000 == 0)
+        {
+            cout << "rearrange" << endl;
+        }
+    }
+
+
+
     return;
 
 }
@@ -67,6 +112,6 @@ void problem2()
 int main()
 {
     problem1();
-    problem2();
+    //problem2();
     return 0;
 }
